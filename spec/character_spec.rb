77e8e58750b1c5558afcb58a1df0868aa8bc3f33
Elliott Pogue <epogue@gmail.com>
2012-08-20 14:30:12 -0400

    Added extra checks to the character spec

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -13,12 +13,18 @@ describe Wowwer::Character do
     its(:name) { should == "Kylarra" }
     its(:realm) { should == "Hyjal" }
     its(:battlegroup) { should == "Whirlwind" }
+    its(:last_modified) { should be_a(Time) }
+
     its(:level) { should be_a(Integer) }
     its(:level) { should == 85 }
-    its(:last_modified) { should be_a(Time) }
+
     its(:thumbnail) { should == "hyjal/217/81894617-avatar.jpg" }
     its(:thumbnail_url) { should == "https://us.battle.net/static-render/us/hyjal/217/81894617-avatar.jpg" }
+
     its(:race) { should be_a(Wowwer::Race) }
+    its(:'race.name') { should == "Draenei" }
+
     its(:character_class) { should be_a(Wowwer::CharacterClass) }
+    its(:'character_class.name') { should == "Shaman" }
   end
 end
